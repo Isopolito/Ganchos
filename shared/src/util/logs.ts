f561fe3475c71d1f905983c35ff303309fefbc79
isopolito <austinshall@yahoo.com>
2020-07-26 19:35:55 -0500

    config directory changes based on env

--- shared/src/util/logs.ts
+++ shared/src/util/logs.ts
@@ -1,14 +1,14 @@
 import * as path from 'path';
-import * as os from 'os';
 import moment from 'moment';
 
 import * as generalConstants from '../constants/names';
+import * as fileUtil from '../util/files';
 
 /*========================================================================================*/
 
 const getFormattedDateForLogs = (): string => moment(new Date()).format("YYYYMMDD");
 
-const makeLogFilePath = (name: string): string => path.join(os.homedir(), generalConstants.AppDir, generalConstants.LogDir, `${getFormattedDateForLogs()}-${name}`);
+const makeLogFilePath = (name: string): string => path.join(fileUtil.getAppBaseDir(), generalConstants.LogDir, `${getFormattedDateForLogs()}-${name}`);
 
 const makeTimeStamp = (timePartOnly?: boolean): string => moment().format(timePartOnly ? "HH:mm:ss" : "MM-DD-YYYY HH:mm:ss");
 
