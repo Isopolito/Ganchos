2d612bd50c99c8f6cc2f9a99c3bd9675c5b8c74c
isopolito <austinshall@yahoo.com>
2020-08-14 07:55:47 -0500

    #23: Clear stale write lock files from log directory on startup

--- shared/src/util/logs.ts
+++ shared/src/util/logs.ts
@@ -1,14 +1,12 @@
 import * as path from 'path';
 import moment from 'moment';
-
-import * as generalConstants from '../constants/names';
 import * as fileUtil from './files';
 
 /*========================================================================================*/
 
 const getFormattedDateForLogs = (): string => moment(new Date()).format("YYYYMMDD");
 
-const makeLogFilePath = (name: string): string => path.join(fileUtil.getAppBaseDir(), generalConstants.LogDir, `${getFormattedDateForLogs()}-${name}`);
+const makeLogFilePath = (name: string): string => path.join(fileUtil.getLogBasePath(), `${getFormattedDateForLogs()}-${name}`);
 
 const makeTimeStamp = (timePartOnly?: boolean): string => moment().format(timePartOnly ? "HH:mm:ss" : "MM-DD-YYYY HH:mm:ss");
 
