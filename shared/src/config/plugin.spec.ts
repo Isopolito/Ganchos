9fab6e27ed14549720d2c9d3d0be5b490746c977
isopolito <austinshall@yahoo.com>
2020-09-04 08:28:30 -0500

    #26: Remove zombie code from plugin config test

--- shared/src/config/plugin.spec.ts
+++ shared/src/config/plugin.spec.ts
@@ -41,16 +41,6 @@ describe('** Plugin Config **', () => {
 
             expect(configObj).to.be.eql({});
         });
-
-        //it('should return  plugin config JSON (even when invalid) if "shouldValidateJson" param is false', async () => {
-        //    const configPath = fileUtil.getPluginConfigPath(pluginName);
-        //    await touch(configPath);
-        //    await fsPromises.writeFile(configPath, '{ bad json }');
-
-        //    const configJson = await pluginConfig.get(pluginName, false);
-
-        //    expect(configJson).to.equal('{ bad json }');
-        //});
     });
 
     describe('When saving a plugin configuration', () => {
@@ -80,19 +70,6 @@ describe('** Plugin Config **', () => {
             expect(fetchedConfigObj.enabled).to.be.undefined;
         });
     });
-
-    //describe(`A call to ${pluginConfig.diffBetweenFileAndMem.name}`, () => {
-    //    it('should accurately report differences between config on disk and in memory', async () => {
-    //        await pluginConfig.save(pluginName, pluginConfigJson);
-    //        pluginConfigObj.propertyOne = 'new prop value';
-    //        const configPath = fileUtil.getPluginConfigPath(pluginName);
-    //        await fsPromises.writeFile(configPath, JSON.stringify(pluginConfigObj, null, 4));
-
-    //        const diffs = await pluginConfig.diffBetweenFileAndMem(pluginName);
-
-    //        expect(diffs).includes('propertyOne');
-    //    });
-    //});
 
     describe(`A call to ${pluginConfig.getJson.name}`, () => {
         it('Should create the plugin config file with the default JSON parameter if the config file does not yet exist', async () => {
