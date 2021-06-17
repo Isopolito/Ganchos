f78d78289791556405266015ff5f83daa4485e2d
isopolito <austinshall@yahoo.com>
2021-06-16 21:21:42 -0500

    #62: finished module loader unit tests

--- engine/src/modules/moduleLoader.spec.ts
+++ engine/src/modules/moduleLoader.spec.ts
@@ -4,12 +4,31 @@ import { promises as fs } from 'fs';
 import { expect } from 'chai'
 import * as sh from 'shelljs'
 
-import { fileUtil, GeneralConfig, systemUtil } from '../shared'
+import { fileUtil, generalConfig, GeneralConfig } from '../shared'
 import { ModuleConfig } from './ModuleConfig'
 import * as moduleLoader from './moduleLoader'
 
-const makeModuleConfigFile = (moduleConfigPath: string): Promise<void> => {
-        const json = `
+const randomConfigFilePath = path.join(fileUtil.getAppBaseDir(), 'randomModuleConfig.json');
+
+const makeModuleConfigFiles = (generalConfigModulePath: string): Promise<[void, void]> => {
+    const randomFileJson = `
+    [
+        {
+            "name": "cwd command random",
+            "path": "cwd",
+            "shouldStart": false,
+            "params": []
+        },
+        {
+            "name": "echo command random",
+            "path": "echo",
+            "shouldStart": true,
+            "params": ["hello random world"]
+        }
+    ]
+    `;
+
+    const generalConfigJson = `
     [
         {
             "name": "dir command",
@@ -32,204 +51,220 @@ const makeModuleConfigFile = (moduleConfigPath: string): Promise<void> => {
     ]
     `;
 
-        return fs.writeFile(moduleConfigPath, json);
+    return Promise.all([fs.writeFile(randomConfigFilePath, randomFileJson),
+    fs.writeFile(generalConfigModulePath, generalConfigJson)]);
 }
 
 describe('** Module Loader **', () => {
-        let config: GeneralConfig;
-        let moduleConfigPath: string;
+    let config: GeneralConfig;
+    let moduleConfigPath: string;
 
-        before(async () => {
-                const testDir = fileUtil.getAppBaseDir();
-                if (testDir.endsWith('test')) sh.rm('-rf', testDir);
-                fileUtil.makeAllDirInPath(testDir);
+    before(async () => {
+        const testDir = fileUtil.getAppBaseDir();
+        if (testDir.endsWith('test')) sh.rm('-rf', testDir);
+        fileUtil.makeAllDirInPath(testDir);
 
-                moduleConfigPath = path.join(testDir, `moduleConfig.json`);
-                await makeModuleConfigFile(moduleConfigPath);
+        moduleConfigPath = path.join(testDir, `moduleConfig.json`);
+        await makeModuleConfigFiles(moduleConfigPath);
 
-                config = {
-                        pluginPaths: [path.join(testDir, `plugins`)],
-                        heartBeatPollIntervalInSeconds: 5,
-                        moduleConfigPath: moduleConfigPath,
-                        pluginScheduleIntervalFloorInMinutes: 0.5,
-                        pluginMetaExtension: 'foo',
-                        eventQueuePluginExecutionTimeout: 0,
-                        eventQueuePluginExecutionConcurrency: 3,
-                        enableDebug: true,
-                        ipChangePollingIntervalInMinutes: 1,
-                        ipUpPollingIntervalInMinutes: 1,
-                };
+        config = {
+            pluginPaths: [path.join(testDir, `plugins`)],
+            heartBeatPollIntervalInSeconds: 5,
+            moduleConfigPath: moduleConfigPath,
+            pluginScheduleIntervalFloorInMinutes: 0.5,
+            pluginMetaExtension: 'foo',
+            eventQueuePluginExecutionTimeout: 0,
+            eventQueuePluginExecutionConcurrency: 3,
+            enableDebug: true,
+            ipChangePollingIntervalInMinutes: 1,
+            ipUpPollingIntervalInMinutes: 1,
+        };
+
+        await generalConfig.save(config);
+    });
+
+    describe('When loading a single module from a Module Config', () => {
+        it('it will start immediately when shouldStart is true', async () => {
+            // arrange 
+            const config: ModuleConfig = {
+                name: "test",
+                path: "/usr/bin/echo", // TODO: make os independent
+                shouldStart: true,
+                params: ["foo"],
+            }
+
+            // act
+            const loadedModule = await moduleLoader.loadSingle(config)
+
+            // assert
+            expect(loadedModule.isInError, `is in error`).to.be.false;
+            expect(loadedModule.isStarted, `is started`).to.be.true;
         });
 
-        describe('Loading a single module from a Module Config', () => {
-                it('should start immediately when shouldStart is true', async () => {
-                        // arrange 
-                        const config: ModuleConfig = {
-                                name: "test",
-                                path: "/usr/bin/sleep", // TODO: make os independent
-                                shouldStart: true,
-                                params: ["5"],
-                        }
+        it('it will NOT start when shouldStart is false', async () => {
+            // arrange 
+            const config: ModuleConfig = {
+                name: "test",
+                path: "/usr/bin/dir", // TODO: make os independent
+                shouldStart: false,
+                params: [],
+            }
 
-                        // act
-                        const loadedModule = await moduleLoader.loadSingle(config)
+            // act
+            const loadedModule = await moduleLoader.loadSingle(config)
 
-                        expect(loadedModule.isInError, `is in error`).to.be.false;
-                        expect(loadedModule.isStarted, `is started`).to.be.true;
-                });
-
-                it('should NOT start when shouldStart is false', async () => {
-                        // arrange 
-                        const config: ModuleConfig = {
-                                name: "test",
-                                path: "/usr/bin/dir", // TODO: make os independent
-                                shouldStart: false,
-                                params: [],
-                        }
-
-                        // act
-                        const loadedModule = await moduleLoader.loadSingle(config)
-
-                        expect(loadedModule.isInError, `is in error`).to.be.false;
-                        expect(loadedModule.isStarted, `is started`).to.be.false;
-                });
-
-                it('should output to stdout when started', async () => {
-                        // arrange 
-                        const config: ModuleConfig = {
-                                name: "test",
-                                path: "/usr/bin/echo", // TODO: make os independent
-                                shouldStart: true,
-                                params: ["Hello World"],
-                        }
-
-                        // act
-                        const loadedModule = await moduleLoader.loadSingle(config)
-                        loadedModule.process.on('data', (data) => {
-                                // assert
-                                expect(data, `output should be captured`).to.equal("Hello World");
-                        });
-
-                        // assert
-                        expect(loadedModule.isStarted, `is started`).to.be.true;
-                        //await systemUtil.waitInSeconds(0.1);
-                });
-
-                it('should be in an error state if module command can not be found', async () => {
-                        // arrange 
-                        const config: ModuleConfig = {
-                                name: "test",
-                                path: "dskjfasdksdfjksdfkjs",
-                                shouldStart: true,
-                                params: [],
-                        }
-
-                        // act
-                        const loadedModule = await moduleLoader.loadSingle(config)
-
-                        // assert
-                        expect(loadedModule.isInError, 'is in error').to.be.true;
-                        expect(loadedModule.isStarted, 'is started').to.be.false;
-                });
+            // assert
+            expect(loadedModule.isInError, `is in error`).to.be.false;
+            expect(loadedModule.isStarted, `is started`).to.be.false;
         });
 
-        describe('Loading multiple modules from a Module Config list', async () => {
-                it('one should start and one should not and neither should have errors', async () => {
-                        // arrange
-                        const configs: ModuleConfig[] = [{
-                                name: "test1",
-                                path: "/usr/bin/echo", // TODO: make os independent
-                                shouldStart: true,
-                                params: ["Hello World"],
-                        },
-                        {
-                                name: "test2",
-                                path: "/usr/bin/dir", // TODO: make os independent
-                                shouldStart: false,
-                                params: [],
-                        }]
+        it('it will output to stdout when started', async () => {
+            // arrange 
+            const config: ModuleConfig = {
+                name: "test",
+                path: "/usr/bin/echo", // TODO: make os independent
+                shouldStart: true,
+                params: ["Hello World"],
+            }
 
-                        // act
-                        const loadedModules = await moduleLoader.load(configs);
-                        const echoModule = loadedModules.find(c => c.name === `test1`);
-                        const dirModule = loadedModules.find(c => c.name === `test2`);
+            // act
+            const loadedModule = await moduleLoader.loadSingle(config)
+            loadedModule.process.on('data', (data) => {
+                // assert
+                expect(data, `output should be captured`).to.equal("Hello World");
+            });
 
-                        // assert
-                        expect(echoModule.isInError, 'is in error').to.be.false;
-                        expect(echoModule.isStarted, 'is started').to.be.true;
-
-                        expect(dirModule.isInError, 'is in error').to.be.false;
-                        expect(dirModule.isStarted, 'is started').to.be.false;
-                });
-
-                it('one should start and one should be in an error state if module command can not be found', async () => {
-                        // arrange
-                        const configs: ModuleConfig[] = [{
-                                name: "test1",
-                                path: "asdfasdfasdfasdf", // TODO: make os independent
-                                shouldStart: true,
-                                params: ["Hello World"],
-                        },
-                        {
-                                name: "test2",
-                                path: "/usr/bin/dir", // TODO: make os independent
-                                shouldStart: true,
-                                params: [],
-                        }]
-
-                        // act
-                        const loadedModules = await moduleLoader.load(configs);
-                        const boinkedModule = loadedModules.find(c => c.name === `test1`);
-                        const dirModule = loadedModules.find(c => c.name === `test2`);
-
-                        // assert
-                        expect(boinkedModule.isInError, 'is in error').to.be.true;
-                        expect(boinkedModule.isStarted, 'is started').to.be.false;
-
-                        expect(dirModule.isInError, 'is in error').to.be.false;
-                        expect(dirModule.isStarted, 'is started').to.be.true;
-                });
-
-                it('one should output to stdout when started, the other is started w/no output; none have errors', async () => {
-                        // arrange
-                        const configs: ModuleConfig[] = [{
-                                name: "test1",
-                                path: "/usr/bin/dir", // TODO: make os independent
-                                shouldStart: true,
-                                params: [],
-                        },
-                        {
-                                name: "test2",
-                                path: "/usr/bin/echo", // TODO: make os independent
-                                shouldStart: true,
-                                params: ["Hello World"],
-                        }]
-
-                        // act
-                        const loadedModules = await moduleLoader.load(configs);
-                        const dirModule = loadedModules.find(c => c.name === `test1`);
-                        const outputModule = loadedModules.find(c => c.name === `test2`);
-
-                        // assert
-                        expect(outputModule.isInError).to.be.false;
-                        outputModule.process.on('data', (data) => {
-                                // assert
-                                expect(data, `output should be captured`).to.equal("Hello World");
-                        });
-
-                        expect(dirModule.isInError, 'in error').to.be.false;
-                        expect(dirModule.isStarted, 'is started').to.be.true;
-                });
+            // assert
+            expect(loadedModule.isStarted, `is started`).to.be.true;
         });
 
-        //describe('Loading multiple modules when explicitly providing a module config file', () => {
-        //}
+        it('it will be in an error state if module command can not be found', async () => {
+            // arrange 
+            const config: ModuleConfig = {
+                name: "test",
+                path: "dskjfasdksdfjksdfkjs",
+                shouldStart: true,
+                params: [],
+            }
 
-        //describe('Loading multiple modules from config specified in ganchos general configuration', () => {
-        //}
+            // act
+            const loadedModule = await moduleLoader.loadSingle(config)
 
-        after(() => {
-                const testDir = fileUtil.getAppBaseDir();
-                if (testDir.endsWith('test')) sh.rm('-rf', testDir);
+            // assert
+            expect(loadedModule.isInError, 'is in error').to.be.true;
+            expect(loadedModule.isStarted, 'is started').to.be.false;
         });
+    });
+
+    describe('When loading multiple modules from a Module Config list', async () => {
+        it('one will start and one will not and neither will have errors', async () => {
+            // arrange
+            const configs: ModuleConfig[] = [{
+                name: "test1",
+                path: "/usr/bin/echo", // TODO: make os independent
+                shouldStart: true,
+                params: ["Hello World"],
+            },
+            {
+                name: "test2",
+                path: "/usr/bin/dir", // TODO: make os independent
+                shouldStart: false,
+                params: [],
+            }]
+
+            // act
+            const loadedModules = await moduleLoader.load(configs);
+            const echoModule = loadedModules.find(c => c.name === `test1`);
+            const dirModule = loadedModules.find(c => c.name === `test2`);
+
+            // assert
+            expect(echoModule.isInError, 'is in error').to.be.false;
+            expect(echoModule.isStarted, 'is started').to.be.true;
+
+            expect(dirModule.isInError, 'is in error').to.be.false;
+            expect(dirModule.isStarted, 'is started').to.be.false;
+        });
+
+        it('one will start and one will be in an error state if module command can not be found', async () => {
+            // arrange
+            const configs: ModuleConfig[] = [{
+                name: "test1",
+                path: "asdfasdfasdfasdf", // TODO: make os independent
+                shouldStart: true,
+                params: ["Hello World"],
+            },
+            {
+                name: "test2",
+                path: "/usr/bin/dir", // TODO: make os independent
+                shouldStart: true,
+                params: [],
+            }]
+
+            // act
+            const loadedModules = await moduleLoader.load(configs);
+            const boinkedModule = loadedModules.find(c => c.name === `test1`);
+            const dirModule = loadedModules.find(c => c.name === `test2`);
+
+            // assert
+            expect(boinkedModule.isInError, 'is in error').to.be.true;
+            expect(boinkedModule.isStarted, 'is started').to.be.false;
+
+            expect(dirModule.isInError, 'is in error').to.be.false;
+            expect(dirModule.isStarted, 'is started').to.be.true;
+        });
+
+        it('one will output to stdout when started and the other will start w/no output; neither will have errors', async () => {
+            // arrange
+            const configs: ModuleConfig[] = [{
+                name: "test1",
+                path: "/usr/bin/dir", // TODO: make os independent
+                shouldStart: true,
+                params: [],
+            },
+            {
+                name: "test2",
+                path: "/usr/bin/echo", // TODO: make os independent
+                shouldStart: true,
+                params: ["Hello World"],
+            }]
+
+            // act
+            const loadedModules = await moduleLoader.load(configs);
+            const dirModule = loadedModules.find(c => c.name === `test1`);
+            const outputModule = loadedModules.find(c => c.name === `test2`);
+
+            // assert
+            expect(outputModule.isInError).to.be.false;
+            outputModule.process.on('data', (data) => {
+                // assert
+                expect(data, `output will be captured`).to.equal("Hello World");
+            });
+
+            expect(dirModule.isInError, 'in error').to.be.false;
+            expect(dirModule.isStarted, 'is started').to.be.true;
+        });
+    });
+
+    describe('When fetching module configurations', () => {
+        it('it will use the path specified in the general config file if one is not provided', async () => {
+            // act
+            const configModules = await moduleLoader.getModuleConfigs();
+
+            // assert
+            expect(configModules.length).to.equal(3);
+        });
+
+        it('it will use the path passed in as a parameter', async () => {
+            // act
+            const configModules = await moduleLoader.getModuleConfigs(randomConfigFilePath);
+
+            // assert
+            expect(configModules.length).to.equal(2);
+        });
+    });
+
+    after(() => {
+        const testDir = fileUtil.getAppBaseDir();
+        if (testDir.endsWith('test')) sh.rm('-rf', testDir);
+    });
 });
